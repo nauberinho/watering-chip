ed6ac2cf0289eb20a27b549b7154fb699af17eb8
nauberinho <nauber91@hotmail.com>
2017-11-17 13:39:33 +0100

    m

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import './main.css';
 import {Provider} from 'react-redux';
 import store from './store/store';
+import { BrowserRouter } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <BrowserRouter history={history}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
     document.getElementById('root'));
 
 registerServiceWorker();
